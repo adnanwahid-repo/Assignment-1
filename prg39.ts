a9bcbb712ac1cee45daeb7e90c1ceb6de359bf47
adnanwahid-repo <160480698+adnanwahid-repo@users.noreply.github.com>
2024-02-19 15:21:54 +0500

    Add files via upload

---
+++ prg39.ts
@@ -0,0 +1,7 @@
+function describe_city(city:string , country:string):void{
+    console.log(city+", "+country);
+}
+
+describe_city("Toronto","Canada");
+describe_city("Lahore", "Pakistan");
+describe_city("Oslo","Norway");
